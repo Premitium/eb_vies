6a61e260bd5f29e29248ab957a08ab835e9f3200
Simeon Parvanov <sp.parvanov.gmail>
2025-08-31 19:00:07 +0300

    Added setup.py, requirements and updated pyproject.toml

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+    install_requires = f.read().splitlines()
+
+
+setup(
+    name='eb_vies',
+    version='0.0.1',
+    description='EU VAT number validation via VIES',
+    author='Your Name',
+    author_email='your@email.com',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+    install_requires=[
+        'zeep>=4.1.0',
+    ]
+)
